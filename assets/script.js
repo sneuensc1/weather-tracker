47b5b9cf39ab5801c200cee89bfd946c7d2efc11
saraneuie <sneuensc@umich.edu>
2022-01-15 12:48:51 -0500

    add api call

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
 
     //get search items from local storage. Use if to set to an empty list if null so there is always a list.
     let weatherList = localStorage.getItem("searchKeyStorage");
@@ -8,13 +8,25 @@ $(function() {
         weatherList = JSON.parse(weatherList);
     }
 
-//pulls the current date and time when the page opens
+    var getWeatherData = function () {
+        //format the weather api
+        var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={minutely, hourly, alerts}&appid={4bfcf3f737f250cacc137acee1f02832}";
+
+        // make a get request to url
+        fetch(apiUrl).then(function(response) {
+            console.log(response);
+            response.json().then(function(data) {
+                console.log(data);
+            });
+        });
+    };
+
+    //pulls the current date and time when the page opens
 let today = new Date();
 
 let currentHour = today.getHours();
 
 $("#currentDay").text(moment().format("Do MMMM YYYY"));
-
 
 
 
